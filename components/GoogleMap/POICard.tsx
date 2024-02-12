be663b1c618e01836ec5747bd6c4f639ab167e6f
Serg <benderlio@gmail.com>
2024-02-12 16:49:37 +0100

    build fix

--- components/GoogleMap/POICard.tsx
+++ components/GoogleMap/POICard.tsx
@@ -5,14 +5,14 @@ type POICardProps = {
 };
 
 export const POICard = ({ poiData }: POICardProps) => {
-  const { name, category, description } = poiData;
+  const { name, category } = poiData;
 
   return (
     <div className="flex flex-col w-[240px] bg-white rounded-xl shadow-lg p-3">
       <div className="flex flex-col flex-grow py-2 gap-3">
         <div className="text-xl ">{name}</div>
         <div className="font-bold">{category}</div>
-        <div className="">{description}</div>
+        {/* <div className="">{description}</div> */}
       </div>
     </div>
   );
